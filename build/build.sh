7c6954bb2c3f4e148312177ad2404b1cf1c76534
Jack Greiner <jack@emoss.org>
2023-01-21 05:50:27 -0500

    Build: Add simple build.sh file.
    Calls dotnet publish and manually copies files.
    TODO:
    use install instead of copy
    add checks for flags, and support for building artemis.plugins.

---
+++ build/build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+VersionNumber=''
+
+function getVersion {
+# Attempt to recreate 'version' job step from gh-actions branch workflow:
+# https://github.com/Artemis-RGB/Artemis/blob/feature/gh-actions/.github/workflows/master.yml
+
+# Should just require 'git', 'grep' and gnu coreutils.
+
+    # Sed-fu the backslashes and periods in branch names.
+    local BranchName=$(git symbolic-ref --short -q HEAD | sed -r 's/[/.]+/-/g')
+
+    # Don't even want to attempt to parse xml in bash without the right tools.. lol
+    local ApiVersion=$(echo "`<./src/Artemis.Core/Artemis.Core.csproj`" | grep -o -P '(?<=<PluginApiVersion>).*(?=</PluginApiVersion>)')
+
+    # Get the number of commits using git rev-list, should be legit afaik
+    local BuildDate=$(date --utc +"%Y-%m-%d")
+    local NumberOfCommitsToday=$(git rev-list --count --after="$BuildDate 00:00" --before="$BuildDate 00:00" HEAD)
+
+    # Assemble Final Version Number
+    VersionNumber="$ApiVersion.$(date +"%Y.%m%d").$NumberOfCommitsToday"
+}
+
+getVersion
+
+echo "BUILDING ARTEMIS"
+dotnet publish --configuration Release -p:Version="$VersionNumber" --runtime "$RUNTIME" --source ./nuget-sources --output build/"$RUNTIME" --self-contained src/Artemis.UI.Linux/Artemis.UI.Linux.csproj
+cp -r --remove-destination /run/build/artemis/build/linux-x64/ /app/bin/
+chmod +x /app/bin/Artemis.UI.Linux
