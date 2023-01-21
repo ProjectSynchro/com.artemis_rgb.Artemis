c8aa8a266e56b5d59a169842d7601b0cfad06445
Jack Greiner <jack@emoss.org>
2023-01-21 09:17:32 -0500

    Build: Add Artemis.Plugins
    I would have done this via another module, but Artemis must be creating it's own nuget packages and I don't know how to move that over to build plugins

--- build/build.sh
+++ build/build.sh
@@ -1,34 +1,86 @@
 #!/usr/bin/env bash
 
-VersionNumber=''
+set -e
 
-function getVersion {
-# Attempt to recreate 'version' job step from gh-actions branch workflow:
-# https://github.com/Artemis-RGB/Artemis/blob/feature/gh-actions/.github/workflows/master.yml
-# Should just require 'git', 'grep' and gnu coreutils.
+# Defaults
+VersionNumber=""
 
-local ApiVersion
-#local BranchName
-local BuildDate
-local NumberOfCommitsToday
-
-    # Sed-fu the backslashes and periods in branch names. (Unused)
-    #BranchName=$(git symbolic-ref --short -q HEAD | sed -r 's/[/.]+/-/g')
-
-    # Don't even want to attempt to parse xml with grep/awk/sed.. lol
-    ApiVersion=$(grep -o -P '(?<=<PluginApiVersion>).*(?=</PluginApiVersion>)' ./src/Artemis.Core/Artemis.Core.csproj)
-
-    # Get the number of commits using git rev-list, should be legit afaik
-    BuildDate=$(date --utc +"%Y-%m-%d")
-    NumberOfCommitsToday=$(git rev-list --count --after="$BuildDate 00:00" --before="$BuildDate 00:00" HEAD)
-
-    # Assemble Final Version Number
-    VersionNumber="$ApiVersion.$(date +"%Y.%m%d").$NumberOfCommitsToday"
+usage() {
+   echo "usage: $(basename "$0") [-v] <verbose output> -h <this help dialog>"
+   cat <<EOF
+Basic build script for use by flatpak-builder when building the Artemis Flatpak.
+Should just require 'git', 'grep' and gnu coreutils.
+EOF
+   exit 1
 }
 
-getVersion
+while getopts vh OPTION "$@"; do
+    case $OPTION in
+    v)
+        set -x
+        ;;
+    h)
+        usage
+        ;;
+    *)
+        usage
+        ;;
+    esac
+done
 
-echo "BUILDING ARTEMIS"
-dotnet publish --configuration Release -p:Version="$VersionNumber" --runtime "$RUNTIME" --source ./nuget-sources --output build/"$RUNTIME" --self-contained src/Artemis.UI.Linux/Artemis.UI.Linux.csproj
-cp -r --remove-destination /run/build/artemis/build/linux-x64/ /app/bin/
-chmod +x /app/bin/Artemis.UI.Linux
+function GetVersion {
+    # Attempt to recreate 'version' job step from gh-actions branch workflow:
+    # https://github.com/Artemis-RGB/Artemis/blob/feature/gh-actions/.github/workflows/master.yml
+    local CoreProjFile
+    local ApiVersion
+    #local BranchName
+    local BuildDate
+    local NumberOfCommitsToday
+
+        # Find Artemis.Core csproj
+        CoreProjFile=$(find "Artemis/src" -type f -name "Artemis.Core.csproj")
+
+        # Sed-fu the backslashes and periods in branch names. (Unused)
+        #BranchName=$(git symbolic-ref --short -q HEAD | sed -r 's/[/.]+/-/g')
+
+        # Don't even want to attempt to parse xml with grep/awk/sed.. lol
+        ApiVersion=$(grep -o -P '(?<=<PluginApiVersion>).*(?=</PluginApiVersion>)' "$CoreProjFile")
+
+        # Get the number of commits using git rev-list, should be legit afaik
+        pushd Artemis
+        BuildDate=$(date --utc +"%Y-%m-%d")
+        NumberOfCommitsToday=$(git rev-list --count --after="$BuildDate 00:00" --before="$BuildDate 00:00" HEAD)
+        popd
+        # Assemble Final Version Number
+        VersionNumber="$ApiVersion.$(date +"%Y.%m%d").$NumberOfCommitsToday"
+}
+    GetVersion
+
+    OutDir="build/build-plugins"
+    StagingDir="build/$RUNTIME"
+    PluginStagingDir="$StagingDir/Plugins"
+
+    mkdir -p "$OutDir" "$StagingDir" "$PluginStagingDir"
+
+    echo "Building Artemis.UI.Linux"
+    UIProjFile=$(find "Artemis/src" -type f -name "Artemis.UI.Linux.csproj")
+    dotnet publish --configuration Release -p:Version="$VersionNumber" --runtime "$RUNTIME" --source ./nuget-sources --output "$StagingDir" --self-contained "$UIProjFile"
+
+    echo "Building Artemis.Plugins"
+    
+    PluginProjects=$(find "Artemis.Plugins/src" -type f -name "*.csproj")
+    for PluginProjFile in $PluginProjects; do
+        Name=$(basename -s .csproj "$PluginProjFile")
+        echo "Building Plugin $Name"
+        Output="$OutDir/$Name"
+
+        dotnet publish --configuration Release --runtime "$RUNTIME" --source ./nuget-sources --output "$Output" --no-self-contained "$PluginProjFile";
+
+        pushd "$Output"
+        zip -r "$Name.zip" .
+        popd
+        mv "$OutDir/$Name/$Name.zip" "$PluginStagingDir"
+    done
+    echo "Staging Artemis Flatpak"
+    chmod +x "$StagingDir/Artemis.UI.Linux"
+    cp -r --remove-destination "$StagingDir" /app/bin/
